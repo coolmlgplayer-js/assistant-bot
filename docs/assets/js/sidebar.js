60711631120ef5be071e0dd1298ad57c55db23a6
CoolMLGPlayer <68787666+coolmlgplayer-js@users.noreply.github.com>
2020-11-28 13:50:10 +0000

    GitHub didnt push the last commit

--- docs/assets/js/sidebar.js
+++ docs/assets/js/sidebar.js
@@ -1,7 +1,7 @@
 function newpage(title,link) { 
   const text = document.createElement("a"); 
-  text.href = link
-  text.innerHTML = title
+  text.href = link;
+  text.innerHTML = title;
 
   const currentDiv = document.getElementById("documents"); 
   currentDiv.after(text,); 
