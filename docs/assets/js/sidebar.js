ccd879e34f72f9599e0e01d5945b9117b910743c
CoolMLGPlayer <68787666+coolmlgplayer-js@users.noreply.github.com>
2020-11-28 14:11:49 +0000

    Update sidebar.js

--- docs/assets/js/sidebar.js
+++ docs/assets/js/sidebar.js
@@ -1,11 +1,13 @@
 function newpage(title,link) { 
   const text = document.createElement("a"); 
   text.href = link;
-  text.innerHTML = title;
+
+  text.innerHTML = title
 
   const currentDiv = document.getElementById("documents"); 
   currentDiv.after(text,); 
 }
-newpage("Download","download");
-newpage("Installing","install");
-newpage("Creating Commands","creatingcommands");
+newpage("Download","download")
+newpage("Installing","install")
+newpage("Creating Commands","creatingcommands")
+
