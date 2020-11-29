69658cf2db9b7c0253e4afdeb7244928311e2b71
CoolMLGPlayer <68787666+coolmlgplayer-js@users.noreply.github.com>
2020-11-29 11:54:54 +0000

    Added contributors

--- src/commands/Information/botinfo.js
+++ src/commands/Information/botinfo.js
@@ -1,5 +1,10 @@
-const { version, name, author, license } = require("../../../package.json");
+const { version, name, author, contributors, license } = require("../../../package.json");
 const { MessageEmbed } = require('discord.js');
+
+var contributorsArr = [];
+contributors.forEach(function(contributor){
+    contributorsArr.push(`[${contributor.name}](${contributor.url})`);
+});
 
 module.exports = {
 	name: 'botinfo',
@@ -13,6 +18,7 @@ module.exports = {
 		.addField("**Version**",version)
 		.addField("**Library**","discord.js v12")
 		.addField("**Creator**",author)
+                .addField("**Contributors**",contributorsArr.join("\n"))
 		.addField("**License**",license)
 		.addField("**Servers**",bot.guilds.cache.array().length);
 		message.reply(embed);
