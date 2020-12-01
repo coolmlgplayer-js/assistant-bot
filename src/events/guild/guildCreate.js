208c9afbcc5cea3f58ec9a6b53118ba1543c08c7
CoolMLGPlayer <68787666+coolmlgplayer-js@users.noreply.github.com>
2020-12-01 11:42:29 +0000

    added fetch members :p

--- src/events/guild/guildCreate.js
+++ src/events/guild/guildCreate.js
@@ -1,3 +1,8 @@
 module.exports = (bot, guild) => {
 	console.log(`New Guild: ${guild.name}`);
+	guild.members.fetch().then(members => {
+		console.log(`${guild.name}: Fetched ${members.size} members!`);
+	}).catch(() => {
+		console.log(`${guild.name}: Error fetching members!`);	
+	});
 };
