7be945188df1544d8ec9cdeebef3bbd2433b82b0
CoolMLGPlayer <68787666+coolmlgplayer-js@users.noreply.github.com>
2020-12-02 15:25:27 +0000

    Changed economy so that it has a wallet and bank :p

--- src/commands/Economy/balance.js
+++ src/commands/Economy/balance.js
@@ -1,4 +1,5 @@
 const db = require('quick.db');
+const { MessageEmbed } = require('discord.js');
 const currency = process.env.currency || 'KoolKoins';
 
 module.exports  = {
@@ -14,7 +15,18 @@ module.exports  = {
         if(args.length > 0) user = User(args.join(' '),message.guild)[0];
         if(!user) throw new Error('User not found!');
         if(user.bot) throw new Error('That user is a bot!');
-        const balance = (db.get(`${user.id}.balance`) || 0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-        return message.reply((user == message.author ? 'You have ' : `**${user.tag}** has `) + `**${balance}** ${currency}`);
+        const walletBalance = (db.get(`${user.id}.economy.wallet`) || 0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        const bankBalance = (db.get(`${user.id}.economy.bank`) || 0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        const bankMax = (db.get(`${user.id}.economy.maxBank`) || 5000).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        const embed = new MessageEmbed({
+            color: message.guild.me.roles.color ? message.guild.me.roles.color.hexColor : message.guild.me.roles.highest.hexColor,
+            author: {
+                name: user.tag,
+                icon_url: user.displayAvatarURL({dynamic: true, format: 'png'})
+            },
+            title: `${currency} Balance`,
+            description: `**Wallet**: ${walletBalance}\n**Bank**: ${bankBalance}/${bankMax}`
+        });
+        return message.reply(embed);
     }
 };
