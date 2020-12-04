da1b67b1656dd9a07961fc9eaf88affe3ff10e85
CoolMLGPlayer <68787666+coolmlgplayer-js@users.noreply.github.com>
2020-12-04 20:20:44 +0000

    Changed help command
     - Removed info command
    
    * Changed help command so it works with commands

--- src/commands/Information/help.js
+++ src/commands/Information/help.js
@@ -4,13 +4,31 @@ module.exports = {
     name: 'help',
     description: 'The help command :p',
     arguments: {
-        optional: ['Category']
+        optional: ['Category/Command']
     },
     async execute(bot, message, args){
         if(args.length > 0){
             var category = args.join(" ");
-            category = bot.categories.find(c => c.toLowerCase() === category.toLowerCase())
-            if(typeof category != "string") throw new Error(`Invalid Category! For a list of categories run \`${prefix}help\``);
+            category = bot.categories.find(c => c.toLowerCase() === category.toLowerCase());
+            const command = bot.commands.get(args.join(" ")) || bot.commands.find(c => c.aliases && c.aliases.includes(args.join(" ")));
+            if(command){
+                const embed = new MessageEmbed({
+                    title: `**${command.name}**`,
+                    color: message.guild.me.roles.color ? message.guild.me.roles.color.hexColor : message.guild.me.roles.highest.hexColor,
+                    description: command.description || 'No description available',
+                });
+                embed.addField("**Category**",command.type);
+                if(command.aliases) embed.addField("**Aliases**", command.aliases.join(", "));
+                if(command.ownerOnly) command.permission = "Bot Owner";
+                if(command.permission) embed.addField("**Permission**",command.permission);
+                if(command.cooldown) embed.addField("**Cooldown**",command.cooldown);
+                if(command.arguments){
+                    if(command.arguments.required) embed.addField("**Required Arguments**","`" + command.arguments.required.join("`, `") + "`")
+                    if(command.arguments.optional) embed.addField("**Optional Arguments**","`" + command.arguments.optional.join("`, `") + "`")
+                };
+                return message.reply(embed);
+            };
+            if(typeof category != "string" && !command) throw new Error(`Invalid Category/Command! For a list of categories run \`${prefix}help\``);
             const commands = bot.commands.filter(function(cmd){
               if(cmd.guild && cmd.guild !== message.guild.id){
               
