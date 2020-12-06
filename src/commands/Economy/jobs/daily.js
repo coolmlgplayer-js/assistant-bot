f0d56c134efd5628848f11060bc8fb2ad5d515d0
CoolMLGPlayer <68787666+coolmlgplayer-js@users.noreply.github.com>
2020-12-06 18:21:44 +0000

    reverted back because i did a stupid

--- src/commands/Economy/jobs/daily.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'daily',
-    minEarn: 2500,
-    maxEarn: 2500,
-    timeOut: '1d',
-    output: 'You have gotten your daily **{coins}** {currency}'
-}
--- src/commands/Economy/shop.js
+++ src/commands/Economy/shop.js
@@ -1,16 +1,6 @@
-const { MessageEmbed, Collection } = require('discord.js');
-const fs = require("fs");
+const { MessageEmbed } = require('discord.js');
 const currency = process.env.currency || 'KoolKoins';
 const { prefix } = process.env;
-const shop = new Collection();
-
-
-const files = fs.readdirSync(`${__dirname}/shop`).filter(x => x.endsWith('.js'));
-for (const file of files){
-    const item = require(`./shop/${file}`);
-    shop.set(item.name, item);
-};
-
 
 module.exports = {
     name: 'shop',
@@ -19,8 +9,9 @@ module.exports = {
         optional: ['Item']
     },
     async execute(bot, message, args){
+        const shop = bot.shop;
         if(args.length > 0){
-            const item = shop.get(args.join(' '));
+            const item = bot.shop.get(args.join(' '));
             if(!item) throw new Error('Invalid item!');
             const embed = new MessageEmbed({
                 title: item.name,
@@ -31,7 +22,7 @@ module.exports = {
             return message.reply(embed);
         }else{
             const items = [];
-            shop.map(item => items.push(item.name));
+            bot.shop.map(item => items.push(item.name));
             const embed = new MessageEmbed({
                 title: 'Shop',
                 color: message.guild.me.roles.color ? message.guild.me.roles.color.hexColor : message.guild.me.roles.highest.hexColor,
