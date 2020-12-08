f64761558784796d4eaf9a2951afde2460d37387
CoolMLGPlayer <68787666+coolmlgplayer-js@users.noreply.github.com>
2020-12-08 22:20:41 +0000

    Added invite page
    why not :p

--- docs/assets/js/sidebar.js
+++ docs/assets/js/sidebar.js
@@ -10,4 +10,4 @@ function newpage(title,link) {
 newpage("Discord Server","https://discord.gg/RWn6Z9aVGj")
 newpage("Creating Commands","creatingcommands")
 newpage("Installing","install")
-newpage("Invite","https://discord.com/api/oauth2/authorize?client_id=772445792182992946&permissions=27670&scope=bot");
+newpage("Invite","invite");
